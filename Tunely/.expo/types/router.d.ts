cf9d9a0ddc10903438a5bbd0f8df4eccb28d92b3
Jason M <46291780+JasonMorta@users.noreply.github.com>
2024-09-28 17:03:00 +0200

    updates

--- Tunely/.expo/types/router.d.ts
+++ Tunely/.expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap` | `/daily` | `/data/events` | `/profile` | `/venues`;
+      StaticRoutes: `/` | `/..\collection\getCollection` | `/..\collection\getEvents` | `/..\components\EventsList` | `/..\config\firebase` | `/..\types\Location` | `/_sitemap` | `/daily` | `/data/events` | `/profile` | `/venues`;
       DynamicRoutes: `/events/${Router.SingleRoutePart<T>}`;
       DynamicRouteTemplate: `/events/[id]`;
     }
